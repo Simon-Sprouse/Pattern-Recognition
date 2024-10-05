6d50ae54402793f2068f27799923c2793d83964c
Simon Sprouse <simonhsprouse@gmail.com>
2024-10-05 15:46:08 -0500

    binary search

--- my-app/src/Choices.js
+++ my-app/src/Choices.js
@@ -1,10 +1,23 @@
-import { useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import hsvToHex from './colorConversions';
 
 function Choices() { 
 
+
+
     const canvasRef1 = useRef(null);
     const canvasRef2 = useRef(null);
+
+    const [color1, setColor1] = useState([0, 100, 100]);
+    const [color2, setColor2] = useState([180, 100, 100]);
+
+    const choice0 = useRef(null);
+    const choice1 = useRef(null);
+
+
+    const clicks = useRef(0);
+
+
 
 
     function fillCanvas(canvasRef, color) { 
@@ -19,49 +32,124 @@ function Choices() {
 
         const choice = event.target.id;
 
-        if (choice == "left") { 
-            const h = Math.floor(Math.random() * 360);
-            fillCanvas(canvasRef1, hsvToHex(h, 100, 100));
+        
+
+
+        if (clicks.current == 0) { 
+
+            if (choice == "left") { 
+                choice0.current = 0;
+            }
+            else if (choice == "right") { 
+                choice0.current = 180;
+            }
+
+            setColor1([90, 100, 100]);
+            setColor2([270, 100, 100]);
         }
-        else if (choice == "right") { 
-            const h = Math.floor(Math.random() * 360);
-            fillCanvas(canvasRef2, hsvToHex(h, 100, 100));
+        else if (clicks.current == 1) { 
+            if (choice == "left") { 
+                choice1.current = 90;
+            }
+            else if (choice == "right") { 
+                choice1.current = 270;
+            }
+
+            if (choice0.current == 0 && choice1.current == 90) { 
+                setColor1([0, 100, 100]);
+                setColor2([90, 100, 100]);
+            }
+            else if (choice0.current == 0 && choice1.current == 270) { 
+                setColor1([270, 100, 100]);
+                setColor2([360, 100, 100]);
+            }
+            else if (choice0.current == 180 && choice1.current == 90) { 
+                setColor1([90, 100, 100]);
+                setColor2([180, 100, 100]);
+            }
+            else if (choice0.current == 180 && choice1.current == 270) { 
+                setColor1([180, 100, 100]);
+                setColor2([270, 100, 100]);
+            }
+
+
         }
+        else { 
+            const leftH = color1[0]; // 180
+            const rightH = color2[0]; // 360
+            const midPoint = Math.floor((rightH + leftH) / 2); // 270
+
+            if (choice == "left") { 
+                // reset right endpoint
+                setColor2([midPoint, 100, 100]);
+            }
+            else if (choice == "right") { 
+                // resset left endpoint
+                setColor1([midPoint, 100, 100]);
+            }
+    
+
+        }
+
+
+        
+
+
+
+
+        
+        clicks.current += 1;
+        
+        
 
     }
 
 
-    useEffect(() => {
-        const canvas1 = canvasRef1.current;
-        const canvas2 = canvasRef2.current;
-        canvas1.addEventListener("click", handleClick);
-        canvas2.addEventListener("click", handleClick);
-
-        
-
-        return () => { 
-            canvas1.removeEventListener("click", handleClick);
-            canvas2.removeEventListener("click", handleClick);
-        }
-    }, []);
-
     useEffect(() => { 
         
-        fillCanvas(canvasRef1, "#ff00ff");
-        fillCanvas(canvasRef2, hsvToHex(200, 100, 100));
+        fillCanvas(canvasRef1, hsvToHex(...color1));
+        fillCanvas(canvasRef2, hsvToHex(...color2));
 
 
-    });
+    }, [color1, color2]);
 
 
     return (
         <>
             <div>
-                <canvas id="left"  ref={canvasRef1} width="400" height="400"></canvas>
-                <canvas id="right" ref={canvasRef2} width="400" height="400"></canvas>
+                <canvas onClick={handleClick} id="left"  ref={canvasRef1} width="400" height="400"></canvas>
+                <canvas onClick={handleClick} id="right" ref={canvasRef2} width="400" height="400"></canvas>
             </div>
         </>
     )
 }
 
 export default Choices;
+
+
+
+
+
+/*
+
+
+Ok so here's what the fuck is happening: 
+
+I need to get this working along one dimension first. 
+
+The whole idea here is to store the h values of every click. 
+
+We then use these h values to predicta good random spot to guess next.
+
+
+The first ten guesses are totally random. 
+
+The winner gets remembered, and the loser goes away. 
+
+
+
+
+
+
+
+*/

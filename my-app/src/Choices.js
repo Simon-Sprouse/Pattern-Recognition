01297262c09491e4acf742c24b7363d92a85c7a1
Simon Sprouse <simonhsprouse@gmail.com>
2024-10-05 03:14:50 -0500

    color selection

--- my-app/src/Choices.js
+++ my-app/src/Choices.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-
+import hsvToHex from './colorConversions';
 
 function Choices() { 
 
@@ -7,30 +7,58 @@ function Choices() {
     const canvasRef2 = useRef(null);
 
 
-    useEffect(() => { 
+    function fillCanvas(canvasRef, color) { 
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = color;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+
+    function handleClick(event) {
+
+        const choice = event.target.id;
+
+        if (choice == "left") { 
+            const h = Math.floor(Math.random() * 360);
+            fillCanvas(canvasRef1, hsvToHex(h, 100, 100));
+        }
+        else if (choice == "right") { 
+            const h = Math.floor(Math.random() * 360);
+            fillCanvas(canvasRef2, hsvToHex(h, 100, 100));
+        }
+
+    }
+
+
+    useEffect(() => {
         const canvas1 = canvasRef1.current;
-        const ctx1 = canvas1.getContext("2d");
-        ctx1.fillStyle = "red";
-        ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
-
-
         const canvas2 = canvasRef2.current;
-        const ctx2 = canvas2.getContext("2d");
-        ctx2.fillStyle = "blue";
-        ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+        canvas1.addEventListener("click", handleClick);
+        canvas2.addEventListener("click", handleClick);
 
+        
 
-
-
-
+        return () => { 
+            canvas1.removeEventListener("click", handleClick);
+            canvas2.removeEventListener("click", handleClick);
+        }
     }, []);
+
+    useEffect(() => { 
+        
+        fillCanvas(canvasRef1, "#ff00ff");
+        fillCanvas(canvasRef2, hsvToHex(200, 100, 100));
+
+
+    });
 
 
     return (
         <>
             <div>
-                <canvas ref={canvasRef1} width="400" height="400"></canvas>
-                <canvas ref={canvasRef2} width="400" height="400"></canvas>
+                <canvas id="left"  ref={canvasRef1} width="400" height="400"></canvas>
+                <canvas id="right" ref={canvasRef2} width="400" height="400"></canvas>
             </div>
         </>
     )
